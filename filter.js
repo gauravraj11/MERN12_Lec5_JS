c03ab7bd0112df090f7d1ffca020e0ebe4a6407a
Gaurav Raj <gaurav.raj@geeksforgeeks.org>
2025-01-18 13:06:28 +0530

    foreach map filter

---
+++ filter.js
@@ -0,0 +1,12 @@
+const A = [-11,-22,0,44,66,55,88,77];
+
+
+const B  = A.filter((i)=>{
+   return i*2;
+   
+})
+
+
+
+console.log(B);
+console.log(A);
---
+++ foreach.js
@@ -0,0 +1,11 @@
+const A = [11,22,33,44,66,55,88,77];
+
+
+ A.forEach((i)=>{
+   console.log(i*2);
+   
+})
+
+
+console.log(A);
+
